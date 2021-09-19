f596dc3b366c7e931005aa9a215b00e826d8842d
Adem <ademtonay@gmail.com>
2021-09-19 16:22:48 +0300

    Create vehicles route for API
    Create /api/vehicles route for API and return fee by vehicles

--- API/router/index.js
+++ API/router/index.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const fees = require("./fees/feesRoutes");
+const vehicles = require("./vehicles/vehiclesRoutes");
 
 router.use("/fees", fees);
+
+router.use("/vehicles", vehicles);
 
 module.exports = router;
