a66d7b7055183751177ab0e951d74208e1f0b810
Adem <ademtonay@gmail.com>
2021-09-19 16:50:08 +0300

    Add bootstrap 5

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,18 @@
 <template>
-  <div id="app"></div>
+  <div id="app">
+    <router-view></router-view>
+  </div>
 </template>
 
 <script>
+import "./assets/libraries/bootstrap/bootstrap.bundle";
 export default {};
 </script>
 
-<style></style>
+<style>
+@import url("./assets/libraries/bootstrap/bootstrap.css");
+#app {
+  width: 100vw;
+  height: 100vh;
+}
+</style>
